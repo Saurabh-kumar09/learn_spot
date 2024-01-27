8f4536598046cc0b687172886753dc670fe84190
Saurabh Kumar <saurabh09.ind@gmail.com>
2024-01-27 23:09:32 +0530

    add

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -7,31 +7,43 @@ class WelcomeScreen extends StatelessWidget {
       child: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Stack(children: [
-          Stack(
-            children: [
-              Container(
+        child: Stack(
+          children: [
+            Stack(
+              children: [
+                Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 1.6,
+                    decoration: const BoxDecoration(color: Colors.white)),
+                Container(
                   width: MediaQuery.of(context).size.width,
                   height: MediaQuery.of(context).size.height / 1.6,
-                  decoration: const BoxDecoration(color: Colors.white)),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 1.6,
-                decoration: const BoxDecoration(
-                  color: Color(0xFF674AEF),
-                  borderRadius:
-                      BorderRadius.only(bottomRight: Radius.circular(70)),
-                ),
-                child: Center(
-                  child: Image.asset(
-                    "images/books.png",
-                    scale: 0.8,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF674AEF),
+                    borderRadius:
+                        BorderRadius.only(bottomRight: Radius.circular(70)),
                   ),
+                  child: Center(
+                    child: Image.asset(
+                      "images/books.png",
+                      scale: 0.8,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 2.666,
+                decoration: BoxDecoration(
+                  color: Color(0Xff674aef),
                 ),
-              )
-            ],
-          ),
-        ]),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
